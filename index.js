1d21494584155e8b81c09b2850f34304282494c4
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-04-20 16:58:46 +0300

    Implemented Storybook

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
+// import App from './.storybook';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import {SafeAreaView} from 'react-native';
 import React from 'react';
+import SearchBar from './components/SearchBar';
+import EventCard from './components/EventCard';
+import data from '../__mocks__/event.json';
 
 export default function App() {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <SearchBar />
+      <EventCard data={data} />
+    </SafeAreaView>
+  );
 }
